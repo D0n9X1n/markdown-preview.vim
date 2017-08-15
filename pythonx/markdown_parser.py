32cfa89ae58a373b57ce0926f4444fc2d13cec9f
MikeCoder <mike@mikecoder.cn>
2017-08-15 11:00:59 +0800

    change the default parser to markdown2

--- pythonx/markdown_parser.py
+++ pythonx/markdown_parser.py
@@ -2,15 +2,15 @@
 # encoding: utf-8
 
 import sys
-import CommonMark
+import markdown2
 
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
 def markdown(text, escape=True):
-    parser = CommonMark.Parser()
-    ast = parser.parse(text)
-
-    renderer = CommonMark.HtmlRenderer()
-    html = renderer.render(ast)
-    return html
+    return markdown2.markdown(text);
+    #  parser = CommonMark.Parser()
+    #  ast = parser.parse(text)
+    #  renderer = CommonMark.HtmlRenderer()
+    #  html = renderer.render(ast)
+    #  return html
