ec62d3a4ad3de6f5be4087174731cfafd184b630
MikeCoder <mike@mikecoder.cn>
2017-08-15 11:07:44 +0800

    pythonx/markdown_parser.py

--- pythonx/markdown_parser.py
+++ pythonx/markdown_parser.py
@@ -2,15 +2,11 @@
 # encoding: utf-8
 
 import sys
-import markdown2
+import markdown_lib
 
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
 def markdown(text, escape=True):
-    return markdown2.markdown(text);
-    #  parser = CommonMark.Parser()
-    #  ast = parser.parse(text)
-    #  renderer = CommonMark.HtmlRenderer()
-    #  html = renderer.render(ast)
-    #  return html
+    parser = markdown_lib.Markdown();
+    return parser.convert(text);
